d3fa8d99f08182741520334374273ec5fc653068
My Git <161316280+Sara-Git1@users.noreply.github.com>
2024-11-14 02:25:59 +0100

    config.php

--- config.php
+++ config.php
@@ -1,27 +1,26 @@
-<?php
-
-$emailErrorMsg=" ";
-$passwordErrorMsg=" ";
+<?php 
+$emailErrorMsg= "";
+$passwordErrorMsg= "";
 
 if(isset($_POST["submit"])){
-
-    $emailValue = $_POST["emailName"];
+    $emailValue = $_POST ["emailName"];
     $passwordValue = $_POST["passName"];
-
     if($emailValue == ""){
-        $emailErrorMsg= " email must be filled";
+        $emailErrorMsg= "email must be filled out";
     }
-    else if (preg_match("/\w+(@emsi.ma){1}$/",$emailValue)==0){
-        $emailErrorMsg= " enter a valid email";
+    else if(preg_match("/\w+(@emsi.ma){1}$/",$emailValue)==0){
+        $emailErrorMsg="  Please enter a valid email adress ";
     }
-    else if ($passwordValue == " "){
-        $passwordErrorMsg= "password must be filled";
+    else if($passwordValue == ""){
+        $passwordErrorMsg= "password must be filled out";
     }
     else {
         session_start();
-        $_SESSION["emailS"] = $emailValue;
+        $_SESSION["emailS"]= $emailValue;
         $_SESSION["passS"]= $passwordValue;
-        header ("Location:home2.php");
+        header("Location : home2.php ");
     }
 }
+
+
 ?>
